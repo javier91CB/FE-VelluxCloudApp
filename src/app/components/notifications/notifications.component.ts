4999a8b4cb0f8130a1084cd3bc91c220d945cd55
Javier Andres Carranza Barragan <Jcarranza@intergrupo.com>
2018-07-19 11:48:08 -0500

    add deploy files web config

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -91,7 +91,95 @@ export class NotificationsComponent implements OnInit {
       Timer: '04:00',
       serviceCall: 1,
       serviceBill: 2
-    }
+    },
+    {
+      Service: 'Sala 1 - 005F',
+      Owner: 'John Doe',
+      Status: TypeStatus.Alert,
+      Timer: '04:00',
+      serviceCall: 1,
+      serviceBill: 2
+    },
+    {
+      Service: 'Sala 1 - 011F',
+      Owner: 'John Doe',
+      Status: TypeStatus.Good,
+      Timer: '02:00',
+      serviceCall: 1,
+      serviceBill: 2
+    },
+    {
+      Service: 'Sala 1 - 006F',
+      Owner: 'John Doe',
+      Status: TypeStatus.Danger,
+      Timer: '15:00',
+      serviceCall: 1,
+      serviceBill: 2
+    },
+    {
+      Service: 'Sala 1 - 001F',
+      Owner: 'John Doe',
+      Status: TypeStatus.Alert,
+      Timer: '05:00',
+      serviceCall: 1,
+      serviceBill: 2
+    },
+    {
+    Service: 'Sala 1 - 005F',
+    Owner: 'John Doe',
+    Status: TypeStatus.Alert,
+    Timer: '04:00',
+    serviceCall: 1,
+    serviceBill: 2
+  },
+  {
+    Service: 'Sala 1 - 011F',
+    Owner: 'John Doe',
+    Status: TypeStatus.Good,
+    Timer: '02:00',
+    serviceCall: 1,
+    serviceBill: 2
+  },
+  {
+    Service: 'Sala 1 - 006F',
+    Owner: 'John Doe',
+    Status: TypeStatus.Danger,
+    Timer: '15:00',
+    serviceCall: 1,
+    serviceBill: 2
+  },
+  {
+    Service: 'Sala 1 - 001F',
+    Owner: 'John Doe',
+    Status: TypeStatus.Alert,
+    Timer: '05:00',
+    serviceCall: 1,
+    serviceBill: 2
+  },
+  {
+    Service: 'Sala 1 - 005F',
+    Owner: 'John Doe',
+    Status: TypeStatus.Alert,
+    Timer: '04:00',
+    serviceCall: 1,
+    serviceBill: 2
+  },
+  {
+    Service: 'Sala 1 - 001F',
+    Owner: 'John Doe',
+    Status: TypeStatus.Alert,
+    Timer: '05:00',
+    serviceCall: 1,
+    serviceBill: 2
+  },
+  {
+    Service: 'Sala 1 - 005F',
+    Owner: 'John Doe',
+    Status: TypeStatus.Alert,
+    Timer: '04:00',
+    serviceCall: 1,
+    serviceBill: 2
+  }
     ];
       this.paginator = true;
   }
