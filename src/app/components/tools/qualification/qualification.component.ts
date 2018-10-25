ce290d6f9a54199df775dbdd71a4dad4d61e9c45
javier91CB <37750494+javier91CB@users.noreply.github.com>
2018-10-25 12:28:03 -0500

    add new fix for the qualification feautre

--- src/app/components/tools/qualification/qualification.component.ts
+++ src/app/components/tools/qualification/qualification.component.ts
@@ -131,4 +131,22 @@ export class QualificationComponent implements OnInit {
     this.notificationInfo.recipiant = '';
     this.selectedOptionPlace = '';
   }
+
+  showDetails(){
+    debugger;
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+  }
 }
