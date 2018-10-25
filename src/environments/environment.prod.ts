d0cc80647f8159aa27adf96ebbc4f4e0e0346281
javier91CB <37750494+javier91CB@users.noreply.github.com>
2018-10-25 01:35:27 -0500

    add new feature

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  
+  firebase : {
+    apiKey: "AIzaSyDBnF-QVzmsFcVhQjS87pguOun9BxSSxKM",
+    authDomain: "ivory-ego-219700.firebaseapp.com",
+    databaseURL: "https://ivory-ego-219700.firebaseio.com",
+    projectId: "ivory-ego-219700",
+    storageBucket: "ivory-ego-219700.appspot.com",
+    messagingSenderId: "1037131669515"
+  }
 };
