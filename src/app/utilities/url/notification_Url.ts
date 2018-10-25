2d6bc56cdea646fc13e6762b6dd25ecd543b8a8f
javier91CB <37750494+javier91CB@users.noreply.github.com>
2018-10-25 14:24:52 -0500

    add configuration for CC

--- src/app/utilities/url/notification_Url.ts
+++ src/app/utilities/url/notification_Url.ts
@@ -1,5 +1,5 @@
 export class notification_Url{
-    // static readonly notificationUrl = "https://vcnotificationapi.azurewebsites.net/api/SendGrid"
+     static readonly notificationUrl = "https://vcnotificationapi.azurewebsites.net/api/SendGrid"
     
-    static readonly notificationUrl = "https://vcdctnnotificationapi.azurewebsites.net/api/SendGrid"
+    //static readonly notificationUrl = "https://vcdctnnotificationapi.azurewebsites.net/api/SendGrid"
 }
