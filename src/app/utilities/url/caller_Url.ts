34dbe27c54c3d3b26486a8efb15bb5c56b99687f
javier91CB <37750494+javier91CB@users.noreply.github.com>
2018-11-01 14:07:23 -0500

    add new features

--- src/app/utilities/url/caller_Url.ts
+++ src/app/utilities/url/caller_Url.ts
@@ -5,6 +5,6 @@ export class caller_Url{
     // static readonly callerHubUrl = 'https://vcdctncallerapi.azurewebsites.net/callerHub';
     // static readonly callerUrl = "https://vcdctncallerapi.azurewebsites.net/api/Buttons";
 
-    //static readonly callerHubUrl = "http://localhost:52978/callerHub";
-    //static readonly callerUrl = "http://localhost:52978/api/Buttons";
+    // static readonly callerHubUrl = "http://localhost:52978/callerHub";
+    // static readonly callerUrl = "http://localhost:52978/api/Buttons";
 }
