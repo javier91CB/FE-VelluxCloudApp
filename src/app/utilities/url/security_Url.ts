4ae8a4d2d9a30fbda9355dbd65794d56807c0388
javier91CB <javiercarranza@javeriana.edu.co>
2018-11-03 22:06:56 -0500

    release decatlon

--- src/app/utilities/url/security_Url.ts
+++ src/app/utilities/url/security_Url.ts
@@ -1,11 +1,11 @@
 export class Security_Url{
-    static readonly permissionUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Permission"
-    static readonly placeUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Place"
-    static readonly userUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/User"
-    static readonly schedulerUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Scheduler"
+    // static readonly permissionUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Permission"
+    // static readonly placeUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Place"
+    // static readonly userUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/User"
+    // static readonly schedulerUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Scheduler"
 
-    // static readonly permissionUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/Permission"
-    // static readonly placeUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/Place"
-    // static readonly userUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/User"
-    // static readonly schedulerUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/Scheduler"
+    static readonly permissionUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/Permission"
+    static readonly placeUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/Place"
+    static readonly userUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/User"
+    static readonly schedulerUrl = "https://vcdctnsecurityapi.azurewebsites.net/api/Scheduler"
 }
