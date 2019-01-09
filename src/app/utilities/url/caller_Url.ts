a4b52a5ad727db0491fbb470c97dc10d46daabd1
javier91CB <javiercarranza@javeriana.edu.co>
2019-01-08 23:33:35 -0500

    changes for cinecolombia

--- src/app/utilities/url/caller_Url.ts
+++ src/app/utilities/url/caller_Url.ts
@@ -1,8 +1,8 @@
 export class caller_Url{
-    static readonly callerHubUrl = 'https://vccallerccwebapi.azurewebsites.net/callerHub';
+    static readonly callerHubUrl = "https://vccallerccwebapi.azurewebsites.net/callerHub";
     static readonly callerUrl = "https://vccallerccwebapi.azurewebsites.net/api/Buttons";
 
-    // static readonly callerHubUrl = 'https://vccallerdctwebapi.azurewebsites.net/callerHub';
+    // static readonly callerHubUrl = "https://vccallerdctwebapi.azurewebsites.net/callerHub";
     // static readonly callerUrl = "https://vccallerdctwebapi.azurewebsites.net/api/Buttons";
 
     // static readonly callerHubUrl = "http://localhost:52978/callerHub";
