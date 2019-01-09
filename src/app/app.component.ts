e51551e2e2c2fa554d93bda787d4df1bee710ef5
javier91CB <javiercarranza@javeriana.edu.co>
2019-01-08 21:38:12 -0500

    add new validation for user login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { MessagingService } from './services/shared/messaging.service';
 import { MenuComponent } from './components/menu/menu.component';
 import { QualificationComponent } from './components/tools/qualification/qualification.component';
 import { observable, fromEvent } from 'rxjs';
+import { RegisterRequest } from './model/users/requestModel/registerRequest';
+import { HelperUserInfo } from './utilities/tools/helperUserInfo';
+import { UserInfoModel } from './model/users/userInfoModel';
+import { TokenModel } from './model/token/tokenModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +19,23 @@ export class AppComponent implements OnInit  {
   // @ViewChild('MenuComponent') childTwo:MenuComponent;
   
   title = 'app';
+  userToEdit: any;
+  userInfoModel: any;
+  tokenModel: any;
 
-  constructor(private messagingService: MessagingService){
+  constructor(private messagingService: MessagingService,
+    private router: Router){
   }
 
   ngOnInit() {
+    this.userToEdit = new RegisterRequest();
+    var _helperUserInfo = new HelperUserInfo();
+    this.userInfoModel = new UserInfoModel();
+    this.tokenModel = new TokenModel();
+    var userInfo = _helperUserInfo.getUserInformation();
+    if(userInfo == null || userInfo == undefined) {
+      this.router.navigate(['/home']);
+    }
     this.messagingService.receiveMessage();
   }
 }
