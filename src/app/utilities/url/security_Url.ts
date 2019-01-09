875b98e3b80eebb2b49ae16ecea79e769143bf50
javier91CB <javiercarranza@javeriana.edu.co>
2019-01-08 23:26:48 -0500

    new branch cinecolombia

--- src/app/utilities/url/security_Url.ts
+++ src/app/utilities/url/security_Url.ts
@@ -1,11 +1,11 @@
 export class Security_Url{
-    // static readonly permissionUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Permission"
-    // static readonly placeUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Place"
-    // static readonly userUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/User"
-    // static readonly schedulerUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Scheduler"
+    static readonly permissionUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Permission"
+    static readonly placeUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Place"
+    static readonly userUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/User"
+    static readonly schedulerUrl = "https://vcsecurityccwebapi.azurewebsites.net/api/Scheduler"
 
-    static readonly permissionUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/Permission"
-    static readonly placeUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/Place"
-    static readonly userUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/User"
-    static readonly schedulerUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/Scheduler"
+    // static readonly permissionUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/Permission"
+    // static readonly placeUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/Place"
+    // static readonly userUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/User"
+    // static readonly schedulerUrl = "https://vcsecuritydctwebapi.azurewebsites.net/api/Scheduler"
 }
