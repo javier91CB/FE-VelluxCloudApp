d7d959bac708c84b256b4a40e3a60a6dbf708b37
javier91CB <javiercarranza@javeriana.edu.co>
2019-01-28 15:56:49 -0500

    add new environments

---
+++ src/environments/environment.QAVM2200.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDBnF-QVzmsFcVhQjS87pguOun9BxSSxKM",
+    authDomain: "ivory-ego-219700.firebaseapp.com",
+    databaseURL: "https://ivory-ego-219700.firebaseio.com",
+    projectId: "ivory-ego-219700",
+    storageBucket: "ivory-ego-219700.appspot.com",
+    messagingSenderId: "1037131669515"
+  },
+  enableLogo: "false",
+  backgroudImage: "",
+  callerHubUrl : "https://vccaller-qa-vm2200webapi.azurewebsites.net/callerHub",
+  callerUrl : "https://vccaller-qa-vm2200webapi.azurewebsites.net/api/Buttons",
+  notificationUrl : "https://vcnotification-qa-vm2200webapi.azurewebsites.net/api/SendGrid",
+  permissionUrl : "https://vcsecurity-qa-vm2200webapi.azurewebsites.net/api/Permission",
+  placeUrl : "https://vcsecurity-qa-vm2200webapi.azurewebsites.net/api/Place",
+  userUrl : "https://vcsecurity-qa-vm2200webapi.azurewebsites.net/api/User",
+  schedulerUrl : "https://vcsecurity-qa-vm2200webapi.azurewebsites.net/api/Scheduler"
+};
